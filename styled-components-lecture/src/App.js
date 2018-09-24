6cd16430cbaba962602bac11f787bdcaaa87d03e
valhentine <138431565+valhentine@users.noreply.github.com>
2018-09-24 11:55:29 -0600

    styles

--- styled-components-lecture/src/App.js
+++ styled-components-lecture/src/App.js
@@ -1,21 +1,84 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import {
+  Header,
+  Button,
+  Segment,
+  Card,
+  Icon,
+  Grid,
+} from 'semantic-ui-react'
+import styled from 'styled-components'
+import axios from 'axios'
+import { HeaderText } from './Typography'
 
-class App extends Component {
+const AppContainer = styled.div`
+  background: linear-gradient(to bottom right, aliceblue, black);
+`
+
+const Transparent = styled(Segment)`
+  background: transparent !important;
+`
+
+const StyledCard = styled(Card)`
+  height: 200px;
+`
+
+const Truncated = styled.div`
+  width: 250px;
+  white-space: nowrap;
+  overflow: hidden;
+  text-overflow: ellipsis;
+`
+
+class App extends React.Component {
+  state = { repos: [] }
+
+  componentDidMount() {
+    //this.getRepos()
+  }
+
+  getRepos = () => {
+    axios.get('https://api.github.com/users/wdjungst/repos?sort=create')
+      .then( res => this.setState({ repos: res.data }) )
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
+      <AppContainer>
+        <Button onClick={this.getRepos}>Get Repos</Button>
+        <HeaderText fSize="large">My Portfolio</HeaderText>
+        <Transparent>
+          <HeaderText>My Projects</HeaderText>
+          <Grid>
+            <Grid.Row>
+              { this.state.repos.map( r =>
+                  <Grid.Column key={r.id} width={4}>
+                    <StyledCard>
+                      <Card.Content>
+                        <Card.Header>
+                          <Truncated>
+                            { r.full_name }
+                          </Truncated>
+                        </Card.Header>
+                        <Card.Meta>
+                          <Truncated>
+                            { r.description }
+                          </Truncated>
+                        </Card.Meta>
+                      </Card.Content>
+                    </StyledCard>
+                  </Grid.Column>
+                )
+              }
+            </Grid.Row>
+          </Grid>
+        </Transparent>
+        <Transparent>
+          <HeaderText fSize="small">Contact</HeaderText>
+        </Transparent>
+      </AppContainer>
+    )
   }
 }
 
-export default App;
+export default App
